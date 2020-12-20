1dee24c78d5467bafcd76633c58f3d6354180740
WillianFarias <williansousafarias@gmail.com>
2020-12-20 17:03:20 -0300

    finalizado

--- js/index.js
+++ js/index.js
@@ -18,16 +18,18 @@ fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?CMC_PRO_API_KEY='
     for (let i = 0; i < 10; i++) {
       //Show API information
       texto = texto + `
-    <div class="media>
-    <img src="coin.png" alt="coin" class="align-self-center mr-3" width="70"
-    height="60">
-      <div class="media-body">
-        <h5 class="mt-2">${api.data[i].name}</h5>
-        <p>${api.data[i].symbol}</p>
-        <p>${api.data[i].first_historical_data}</p>
+                  
+      <div class="media">
+          <img src="coin.png" class="align-self-center mr-3" alt="coin" width="45" height="30">
+          <div class="media-body">
+          <h5 class="mt-2">${api.data[i].name}</h5>
+          <p>${api.data[i].symbol}</p>
+          <p>${api.data[i].first_historical_data}</p>
+          </div>
       </div>
-    </div>
+ 
       `;
+
       document.getElementById("coins").innerHTML = texto;
     }
   })
